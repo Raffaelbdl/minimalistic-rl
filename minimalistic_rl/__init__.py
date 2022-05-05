2424bbe2213959d3d41dd4e1261f351202e692f7
Raffaelbdl <raffaelbdl@gmail.com>
2022-05-05 10:15:09 +0200

    :pencil: Add __init__.py

---
+++ minimalistic_rl/__init__.py
@@ -0,0 +1,2 @@
+from minimalistic_rl.buffer import Buffer
+from minimalistic_rl.utils.updater import apply_updates
---
+++ minimalistic_rl/algorithms/__init__.py
@@ -0,0 +1,3 @@
+from minimalistic_rl.algorithms.base import Base
+from minimalistic_rl.algorithms.dqn import DQN
+from minimalistic_rl.algorithms.ppo import PPO
