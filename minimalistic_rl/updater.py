d4d3e8f03e56e6acd453d203ddbe174be3682a9d
Raffaelbdl <raffaelbdl@gmail.com>
2022-05-06 22:52:52 +0200

    :truck: Rm utils

--- minimalistic_rl/updater.py
+++ minimalistic_rl/updater.py
@@ -1,12 +1,20 @@
 import functools
 
+import chex
+import haiku as hk
 import jax
-from jax import numpy as jnp
 import optax
+
+Grads = chex.ArrayTree
 
 
 @functools.partial(jax.jit, static_argnums=(0))
-def apply_updates(optimizer, params, opt_state, grads):
+def apply_updates(
+    optimizer: optax.GradientTransformation,
+    params: hk.Params,
+    opt_state: optax.OptState,
+    grads: Grads,
+):
     updates, opt_state = optimizer.update(grads, opt_state, params)
     params = optax.apply_updates(params, updates)
     return params, opt_state
