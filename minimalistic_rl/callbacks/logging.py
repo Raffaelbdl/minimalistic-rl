cd2c78d386ae07732fa5f5fefd9900d385b71942
Raffaelbdl <raffaelbdl@gmail.com>
2022-05-07 22:37:00 +0200

    :sparkles: Add verbose to logger

--- minimalistic_rl/callbacks/logging.py
+++ minimalistic_rl/callbacks/logging.py
@@ -1,3 +1,4 @@
+from collections import deque
 import logging
 
 from minimalistic_rl.callbacks.callback import Callback
@@ -8,12 +9,28 @@ EPISODE_FMT = "%(asctime)s | EPISODE | %(message)s"
 
 
 class Logger(Callback):
-    def __init__(self, verbose: int):
+    """Logs information during training
+
+    Verbose levels :
+    - 0 : no logs
+    - 1 : logs at each episode cycle
+    - 2 : logs at each episode
+    - 3 : logs at each step
+    """
+
+    def __init__(self, config: dict):
 
         self.logger = init_logger("minimalistic-rl/logger", DEFAULT_FMT, logging.INFO)
-        self.verbose = verbose
+        self.verbose = config["verbose"]
+
+        if self.verbose == 1:
+            self.rewards = deque(maxlen=config["episode_cycle_len"])
 
     def at_train_start(self, logs: dict):
+
+        fmtter = logging.Formatter(DEFAULT_FMT)
+        self.logger.handlers[0].setFormatter(fmtter)
+
         self.logger.info("Training is starting !")
 
         algo = logs["algo"]
@@ -21,15 +38,47 @@ class Logger(Callback):
         self.logger.info(f"Algo is {algo}, training for {n_steps} steps")
 
     def at_train_end(self, logs: dict):
+
+        fmtter = logging.Formatter(DEFAULT_FMT)
+        self.logger.handlers[0].setFormatter(fmtter)
+
         self.logger.info("Training has finished !")
 
     def at_episode_end(self, logs: dict):
+
         fmtter = logging.Formatter(EPISODE_FMT)
         self.logger.handlers[0].setFormatter(fmtter)
 
-        ep_count = logs["ep_count"]
-        ep_reward = logs["ep_reward"]
-        self.logger.info(f"Episode is {ep_count}, reward is {ep_reward}")
+        if self.verbose == 1:
+
+            ep_count = logs["ep_count"]
+            ep_reward = logs["ep_reward"]
+            self.rewards.append(ep_reward)
+            if ep_count % self.rewards.maxlen == 0:
+                mean_reward = sum(self.rewards) / len(self.rewards)
+                self.logger.info(
+                    f"At episode {ep_count} > mean_reward is {mean_reward:.1f}"
+                )
+
+        elif self.verbose >= 2:
+
+            ep_count = logs["ep_count"]
+            ep_reward = logs["ep_reward"]
+            self.logger.info(f"At episode {ep_count} > reward is {ep_reward:.1f}")
+
+        fmtter = logging.Formatter(DEFAULT_FMT)
+        self.logger.handlers[0].setFormatter(fmtter)
+
+    def at_step_end(self, logs: dict):
+
+        fmtter = logging.Formatter(STEP_FMT)
+        self.logger.handlers[0].setFormatter(fmtter)
+
+        if self.verbose == 3:
+
+            step_count = logs["step_count"]
+            step_reward = logs["step_reward"]
+            self.logger.info(f"At step {step_count} > reward id {step_reward:.1f}")
 
         fmtter = logging.Formatter(DEFAULT_FMT)
         self.logger.handlers[0].setFormatter(fmtter)
