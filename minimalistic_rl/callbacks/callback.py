bc330d7d08404894cd3a13df38792bb59978e7e8
Raffaelbdl <raffaelbdl@gmail.com>
2022-05-07 22:36:41 +0200

    :sparkles: Add calls to callback

--- minimalistic_rl/callbacks/callback.py
+++ minimalistic_rl/callbacks/callback.py
@@ -1,6 +1,3 @@
-import logging
-
-
 class Callback:
     def at_train_start(self, logs: dict):
         pass
@@ -8,5 +5,14 @@ class Callback:
     def at_train_end(self, logs: dict):
         pass
 
+    def at_episode_start(self, logs: dict):
+        pass
+
     def at_episode_end(self, logs: dict):
+        pass
+
+    def at_step_start(self, logs: dict):
+        pass
+
+    def at_step_end(self, logs: dict):
         pass
