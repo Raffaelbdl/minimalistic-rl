b574c2d6b8ac2b168cf42c13bcbb99f5d15ba019
Raffaelbdl <raffaelbdl@gmail.com>
2022-07-02 21:30:09 +0200

    :zap: Change trainer

--- minimalistic_rl/trainer.py
+++ minimalistic_rl/trainer.py
@@ -1,40 +1,50 @@
 """Simple training loop"""
+import dataclasses
 import functools
+from gc import callbacks
 from typing import List, Optional, Union
 
-import chex
-import gym
-import jax.random as jrng
+import jax
 import numpy as np
 from tqdm import tqdm
 
-from minimalistic_rl import algorithms as algo
+from minimalistic_rl.agents import base
 from minimalistic_rl.buffer import from_singles
-from minimalistic_rl.callbacks import Callback, Logger
+from minimalistic_rl.callbacks import Callback, Logger, callback
 from minimalistic_rl.wrapper import VecEnv
 
 
-Array = chex.Array
-ArrayNumpy = chex.ArrayNumpy
-PRNGKey = chex.PRNGKey
-Scalar = chex.Scalar
+@dataclasses.dataclass
+class TrainingConfig:
+    """Configuration for training
+
+    Attributes:
+        seed (int)
+        verbose (int)
+        episode_cycle_length (int)
+        render (bool)
+    """
+
+    seed: int = 0
+    verbose: int = 0
+    episode_cycle_length: int = 10
+    render: bool = False
 
 
-def train(
-    rng: PRNGKey,
-    agent: algo.Base,
-    env: Union[gym.Env, VecEnv],
+def _train(
+    agent: base.Base,
     callbacks: Optional[List[Callback]] = None,
-    render: bool = False,
+    training_config: TrainingConfig = Optional[None],
 ):
-    try:
-        num_envs = env.num_envs
-    except:
-        env = VecEnv(env, 1)
-    config = agent.config
+
+    training_config = (
+        training_config if training_config is not None else TrainingConfig()
+    )
+    training_config.num_envs = agent.num_envs
+    training_config.num_env_steps = agent.num_env_steps
+
     logs = init_logs(agent, env.num_envs)
-    config["num_envs"] = env.num_envs
-    callbacks = init_callbacks(config, callbacks)
+    callbacks = init_callbacks(training_config, callbacks)
 
     if not isinstance(env, VecEnv):
         env = VecEnv(env, 1)
@@ -103,40 +113,108 @@ def train(
     return logs
 
 
-def off_policy_improve_condition(
-    step: int, agent: algo.Base, improve_cycle: int, batch_size: int
-) -> bool:
-    return step % improve_cycle == 0 and len(agent.buffer) > batch_size
+def train(
+    agent: base.Base,
+    training_config: TrainingConfig = None,
+    callbacks: List[Callback] = None,
+):
+    training_config = (
+        training_config if training_config is not None else TrainingConfig()
+    )
+    key = jax.random.PRNGKey(training_config.seed)
+    envs = agent.envs
 
+    training_config.num_env_steps = agent.num_env_steps
+    training_config.num_envs = agent.num_envs
+    num_steps = agent.num_env_steps // agent.num_envs
+    training_config.num_steps = num_steps
 
-def on_policy_improve_condition(step: int, agent: algo.Base, T: int) -> bool:
-    return len(agent.buffer) >= T
+    callbacks = init_callbacks(training_config, callbacks)
+    logs = init_logs(agent)
 
+    observations = envs.reset()
+    for step in range(num_steps):
+        global_step = int(step * agent.num_envs)
+        logs["step_count"] = global_step
 
-def init_logs(agent: algo.Base, num_envs) -> dict:
-    logs = {
-        "algo": agent.algo,
-        "policy": agent.policy,
-        "ep_count": 0,
-        "ep_reward": [0.0 for _ in range(num_envs)],
-        "num_envs": num_envs,
-        "total_loss": 0.0,
-        "last_ep_reward": 0.0,
-    }
-    if agent.algo == "ppo":
-        logs.update(
-            {"actor_loss": 0.0, "critic_loss": 0.0, "entropy": 0.0, "approx_kl": 0.0}
+        key = jax.random.split(key)[0]
+
+        actions, logps = agent.select_action(key, observations)
+        next_observations, rewards, dones, infos = envs.step(actions)
+        logs["step_reward"] = rewards
+        logs["episodic_reward"] += rewards
+
+        agent.buffer.add(
+            from_singles(
+                observations, actions, rewards, dones, next_observations, logps
+            )
         )
-    logs.update(agent.config)
+
+        if agent.policy_type == "on":
+            if on_policy_improve_condition(step, agent):
+                logs = agent.improve(logs)
+        elif agent.policy_type == "off":
+            if off_policy_improve_condition(step, agent, 1, 1):
+                raise NotImplementedError()
+
+        for i, d in enumerate(dones):
+            if d:
+                for c in callbacks:
+                    logs["episode_count"] += 1
+                    logs["last_ended"] = i
+                    c.at_episode_end(logs)
+                logs["episodic_reward"][i] = 0.0
+
+        observations = next_observations
+
+        for c in callbacks:
+            c.at_step_end(logs)
+
+    return logs
+
+
+def off_policy_improve_condition(
+    step: int, agent: base.Base, improve_cycle: int, batch_size: int
+) -> bool:
+    raise NotImplementedError()
+    # return step % improve_cycle == 0 and len(agent.buffer) > batch_size
+
+
+def on_policy_improve_condition(step: int, agent: base.OnPolicy) -> bool:
+    return len(agent.buffer) >= agent.agent_config.num_buffer_steps
+
+
+def init_logs(agent: base.Base) -> dict:
+    logs = {
+        "agent_name": agent.agent_name,
+        "policy_type": agent.policy_type,
+        "global_step": 0,
+        "episode_count": 0,
+        "last_ended": 0,
+        "step_count": 0,
+        "step_reward": np.zeros((agent.num_envs,), dtype=np.float32),
+        "episodic_reward": np.zeros((agent.num_envs,), dtype=np.float32),
+        "num_updates": 0,
+    }
+
+    if agent.agent_name == "ppo":
+        logs.update(
+            {
+                "total_loss": 0.0,
+                "actor_loss": 0.0,
+                "critic_loss": 0.0,
+                "entropy": 0.0,
+                "approx_kl": 0.0,
+            }
+        )
 
     return logs
 
 
 def init_callbacks(
-    config: dict, callbacks: Optional[List[Callback]] = None
+    callbacks_config: dict, callbacks: Optional[List[Callback]] = None
 ) -> List[Callback]:
     callbacks = callbacks if callbacks else []
-    callbacks.append(Logger(config))
+    callbacks.append(Logger(callbacks_config))
 
     return callbacks
-
