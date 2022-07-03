af345fa8c06a8b4ef0d1dbeb0466c8e8dd3daf7c
Raffaelbdl <raffaelbdl@gmail.com>
2022-07-03 08:36:17 +0200

    :sparkles: Add wandb callback

--- minimalistic_rl/callbacks/__init__.py
+++ minimalistic_rl/callbacks/__init__.py
@@ -1,2 +1,2 @@
-from minimalistic_rl.callbacks.callback import Callback
+from minimalistic_rl.callbacks.callback import Callback, WandbCallback
 from minimalistic_rl.callbacks.logging import Logger
--- minimalistic_rl/callbacks/callback.py
+++ minimalistic_rl/callbacks/callback.py
@@ -1,3 +1,9 @@
+from collections import deque
+from typing import List
+
+import wandb
+
+
 class Callback:
     def at_train_start(self, logs: dict):
         pass
@@ -16,3 +22,21 @@ class Callback:
 
     def at_step_end(self, logs: dict):
         pass
+
+
+class WandbCallback(Callback):
+    def __init__(self, log_keys: List[str] = None):
+        self.log_keys = (
+            log_keys
+            if log_keys is not None
+            else ["step_count", "episodic_return", "total_loss"]
+        )
+
+    def at_episode_end(self, logs: dict):
+        wandb_log = {}
+        for key in self.log_keys:
+            if "mean" in key and isinstance(logs[key], deque):
+                wandb_log[key] = sum(logs[key]) / len(logs[key])
+            else:
+                wandb_log[key] = logs[key]
+        wandb.log(wandb_log)
